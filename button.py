d90bd459e8539e908354147e37befb1f2f28c676
Flatbaby <60462768+Flatbaby@users.noreply.github.com>
2022-10-16 22:17:32 +0200

    s

--- button.py
+++ button.py
@@ -9,21 +9,9 @@ class Button():
         
         
     def draw(self, surface):
-        action = False
-        #mouse position
-        pos = pygame.mouse.get_pos()
-        
-        #check mouseover and clicked
-        if self.rect.collidepoint(pos):
-            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False :
-                self.clicked = True
-                action = True
-        if pygame.mouse.get_pressed()[0] == 0 :
-            self.clicked = False
-        
+ 
         surface.blit(self.image, (self.rect.x, self.rect.y))
         
-        return action
         
     
 
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import button
 pygame.init()
 pygame.font.init()
 
-WIDTH, HEIGHT = 800,600
+WIDTH, HEIGHT = 1024,800
 
 
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 
 #image load 
 btn_load = pygame.image.load('button.png').convert_alpha()
-btn = pygame.transform.scale(btn_load, (100, 100))
+btn = pygame.transform.scale(btn_load, (90, 90))
 
 MONEY = 0
 WHITE = (255,255,255)
@@ -30,22 +30,27 @@ FPS = 60
 
 
 
-start_button = button.Button(HEIGHT/2, WIDTH/2, btn)
+start_button = button.Button(470, 360,btn)
 
 
+def rectangle(display, color, x, y, w, h):
+    pygame.draw.rect(display, color, (x, y, w, h))
+    
 
 def draw_window():
     
     WIN.fill(WHITE)
-    if start_button.draw(WIN):
-        MONEY =+ 1
-        print(MONEY)
+    start_button.draw(WIN)
+  
             
     pygame.display.update()
     
 def main():
     clock = pygame.time.Clock()
     run = True
+    
+    mong = 1
+    coins = 1
     
     
     
@@ -56,6 +61,12 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mopos = pygame.mouse.get_pos()
+                if mopos >= (380,0):
+                    if mopos <= (560,0):
+                        coins += mong
+                        print(coins)
        
           
         draw_window() 
